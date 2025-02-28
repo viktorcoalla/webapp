2d7ba359b04ff23f4c36a6074b061407538461a1
viktorcoalla <112937424+viktorcoalla@users.noreply.github.com>
2025-02-28 11:15:30 +0300

    Update swup.js

--- swup.js
+++ swup.js
@@ -2,14 +2,12 @@ import Swup from 'https://unpkg.com/swup@4?module';
 const swup = new Swup({
   containers: ["#swup"]
 });
-swup.hooks.on('visit:start', () => {
-  console.log(window.location.href);
-})
-
-
 const swup2 = new Swup({
   containers: ["#allrecords"]
 });
 swup.hooks.on('visit:start', () => {
   console.log(window.location.href);
 })
+
+
+
